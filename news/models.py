09fb9bdf60425db8e06902a9ab75147376dab3aa
MuradBabayev-Git <a.murad.babayev@gmail.com>
2025-05-03 20:34:04 +0400

    добавил все продукты.если вы скачали и не увидели нечего в админке и на сайте,то это не моя вина я все щас добавил ПРЯМ ВСЕgit add .!

--- news/models.py
+++ news/models.py
@@ -113,7 +113,7 @@ class Developer(models.Model):
 class About_us(models.Model):
     title = models.CharField(max_length=255, verbose_name="Title")
     description = models.TextField(blank=True, verbose_name="Content", null=True)
-    image = models.ImageField(upload_to="photo/%Y/%m/%d/", verbose_name="Image", null=True, blank=True)
+    # image = models.ImageField(upload_to="photo/%Y/%m/%d/", verbose_name="Image", null=True, blank=True)
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Time_create", null=True)
     time_update = models.DateTimeField(auto_now=True, verbose_name="Time_update", null=True)
     is_published = models.BooleanField(default=True, verbose_name="Status", null=True)
